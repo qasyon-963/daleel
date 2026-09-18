8b128c4728ba08a519ad9741b71734c04550fa5f
qasyon-963 <abdo.art.963@gmail.com>
2026-09-18 19:45:34 +0300

    Trigger APK build

---
+++ android/app/src/main/java/com/syria/daleel/MainActivity.java
@@ -0,0 +1,5 @@
+package com.syria.daleel;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.syria.daleel',
+  appName: 'Daleel',
+  webDir: 'dist'
+};
+
+export default config;
